bc25859e255238dc6c8750242f884991d919a6ab
Baibol123 <93245069+Baibol123@users.noreply.github.com>
2024-01-04 03:52:28 +0600

    Update MainActivity.kt

--- app/src/main/java/com/example/homework_m3_7/MainActivity.kt
+++ app/src/main/java/com/example/homework_m3_7/MainActivity.kt
@@ -41,16 +41,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun loadData(listSongs: ArrayList<Song>) {
         this.listSongs.apply {
-            add(Song("Last Christmas", "Wham", 170))
-            add(Song("Raise Your Glass", "P!nk", 158))
-            add(Song("Happy Xmas (War Is Over)", "John Lennon", 210))
-            add(Song("Feliz Navidad", "Jose Feliciano", 195))
-            add(Song("Jingle Bell Rock", "Bobby Helms", 201))
-            add(Song("The Parting Glass", "Hozier", 187))
-            add(Song("Wonderful Christmastime", "Paul McCartney", 233))
+            add(Song("Last Christmas", "Wham", 150))
+            add(Song("Raise Your Glass", "P!nk", 108))
+            add(Song("Happy Xmas (War Is Over)", "John Lennon", 150))
+            add(Song("Feliz Navidad", "Jose Feliciano", 290))
+            add(Song("Jingle Bell Rock", "Bobby Helms", 120))
+            add(Song("The Parting Glass", "Hozier", 177))
+            add(Song("Wonderful Christmastime", "Paul McCartney", 222))
             add(Song("Happy New Year", "Judy Garland", 222))
-            add(Song("Do They Know It's Christmas?", "Band Aid", 256))
-            add(Song("Funky New Year", "Eagles", 149))
+            add(Song("Do They Know It's Christmas?", "Band Aid", 199))
+            add(Song("Funky New Year", "Eagles", 188))
         }
     }
     companion object {
